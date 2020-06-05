cf62e492f2c2a64834f8ffc1a079912123cf5d1e
sarigu <32572115+sarigu@users.noreply.github.com>
2020-06-05 02:41:40 +0200

    changed overall styling

--- app.js
+++ app.js
@@ -4,29 +4,12 @@ document.addEventListener("mousemove", function(event) {
   const x = event.pageX;
   const y = event.pageY;
 
-  document.querySelectorAll("#flower-container div").forEach(div => {
+  document.querySelectorAll("#hero-container div").forEach(div => {
     const dx = div.offsetLeft + 50 - x; // weil 50 von dem linken punkt ist die mitte
     const dy = div.offsetTop + 50 - y;
     const dist = Math.sqrt(dx * dx + dy * dy);
-    const score = Math.exp(dist * -0.004);
+    const score = Math.exp(dist * -0.007);
 
     div.style.transform = "scale(" + score + ")";
   });
-});
-
-const slideshow = document.querySelector("#slideshow");
-const images = slideshow.querySelectorAll("#slideshow img");
-
-slideshow.addEventListener("mousemove", function(event) {
-  const x = event.offsetX;
-  const width = this.offsetWidth;
-
-  const percentage = x / width;
-  const imageNumber = Math.floor(percentage * images.length);
-
-  images.forEach(image => {
-    image.style.zIndex = 0;
-  });
-
-  images[imageNumber].style.zIndex = 1;
 });
